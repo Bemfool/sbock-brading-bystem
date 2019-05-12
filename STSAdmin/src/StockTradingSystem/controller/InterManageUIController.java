9f1d42b2b95b3c819a45aaaffdc63435979e0f7f
Keith Lin <3160104229@zju.edu.cn>
2019-05-12 23:31:37 +0800

    增加界面跳转和设置涨跌/状态输入段

--- STSAdmin/src/StockTradingSystem/controller/InterManageUIController.java
+++ STSAdmin/src/StockTradingSystem/controller/InterManageUIController.java
@@ -1,7 +1,17 @@
 package StockTradingSystem.controller;
 
+import javafx.event.ActionEvent;
+
 public class InterManageUIController extends AdminUIController {
 
     public void modifyPassword() {
+    }
+
+    public void gotoMainUI() throws Exception {
+        super.getApp().gotoAdminMainUI();
+    }
+
+    public void gotoInterManage() throws Exception {
+        super.getApp().gotoInternalManageUI();
     }
 }
